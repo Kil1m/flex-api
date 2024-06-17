832bc9a31f0836679e396a49938325e8e15df400
Kil1m <2413574506@qq.com>
2024-06-17 12:13:08 +0800

    init

---
+++ src/main/java/com/kilim/kilim_flex_api/KilimFlexApiApplication.java
@@ -0,0 +1,13 @@
+package com.kilim.kilim_flex_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KilimFlexApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KilimFlexApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kilim/kilim_flex_api/KilimFlexApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kilim.kilim_flex_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KilimFlexApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
